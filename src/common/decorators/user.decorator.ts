a967bd417d66f16b54cac768ef63a8f53ddddadb
Marcius <marciusdasilva51@gmail.com>
2023-09-24 13:21:26 +0100

    Refact: User decorator

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -3,12 +3,12 @@ import { Socket } from "socket.io";
 
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
-    if (ctx.getType() !== "http") {
+    if (ctx.getType() === "http") {
       const request = ctx.switchToHttp().getRequest();
       const user = request.user;
       return data ? user?.[data] : user;
     }
-    if (ctx.getType() !== "ws") {
+    if (ctx.getType() === "ws") {
       const client: Socket = ctx.switchToWs().getClient();
       const { authorization } = client.handshake.headers;
       return authorization;
